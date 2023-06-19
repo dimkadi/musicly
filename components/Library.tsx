4fb357fc0fa890a90f50bd760ec994df074d24e8
Дмитрий Дьячков (Dmitri Diakov) <dmitridiakov@gmail.com>
2023-06-19 21:44:39 +0300

    init app upload commit

---
+++ components/Library.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { AiOutlinePlus } from 'react-icons/ai';
+import { TbPlaylist } from 'react-icons/tb';
+
+import useUploadModal from '@/hooks/useUploadModal';
+import useAuthModal from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUser';
+import { Song } from '@/types';
+import MediaItem from './MediaItem';
+import useOnPlay from '@/hooks/useOnPlay';
+
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({
+  songs
+}) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
+
+  const onClick = () => {;
+  if(!user) {
+    return authModal.onOpen();
+    }
+    return uploadModal.onOpen();
+  };
+  return (
+    <div className='flex flex-col'>
+      <div
+        className='
+            flex
+            items-center
+            justify-between
+            px-5
+            pt-4
+            '
+      >
+        <div
+          className='
+                inline-flex
+                items-center
+                gap-x-2
+                '
+        >
+          <TbPlaylist className='text-neutral-400' size={26} />
+          <p
+            className='
+                    text-neutral-400
+                    text-md
+                    font-medium
+                    '
+          >
+            Your Library
+          </p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className='text-neutral-400 cursor-pointer hover:text-white transition'
+        />
+      </div>
+      <div className='
+      flex
+        flex-col
+        gap-y-2
+        px-3
+        mt-4
+      '>
+        {songs.map((item) => (
+          <MediaItem
+          onClick={(id: string) => onPlay(id)}
+          key={item.id}
+          data={item}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
---
+++ components/SearchInput.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import qs from 'query-string';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import useDebounce from '@/hooks/useDebounce';
+import Input from './Input';
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue
+    };
+    const url = qs.stringifyUrl({
+      url: '/search',
+      query: query
+    });
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder='Search for a song...'
+      value={value}
+      onChange={e => setValue(e.target.value)}
+    />
+  );
+};
+
+export default SearchInput;
